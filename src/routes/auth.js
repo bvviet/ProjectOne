a142391a82ab065cd552871d82e30c1aec30d7c3
bvviet <lanviethoang2004@thoang2004gmail.g2004gmailcom>
2024-06-29 19:38:07 +0700

    fig get detail user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ import AuthController from "../controllers/auth.js";
 
 const authRouter = Router();
 
-authRouter.post("/:id", AuthController.getDetailUser);
+authRouter.get("/:id", AuthController.getDetailUser);
 authRouter.post("/register", AuthController.register);
 authRouter.post("/login", AuthController.login);
 
